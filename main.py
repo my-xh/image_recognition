eadadf67715c5570f633a7afcb92e29987bd5a1f
xuheng <2435128850@qq.com>
2021-04-23 21:08:51 +0800

    设计主界面和控件功能

--- main.py
+++ main.py
@@ -8,3 +8,60 @@
 @Software: PyCharm
 @Desc    : 
 """
+
+import os
+import sys
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, QSize
+from main_window import Ui_MainWindow
+
+
+class MyUi(QMainWindow, Ui_MainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setup()
+
+    def setup(self):
+        self.btn_select.clicked.connect(self.select_image)
+        self.cb_type.currentIndexChanged.connect(self.select_type)
+        self.btn_copy.clicked.connect(self.copy_result)
+
+    def select_image(self):
+        """打开文件选择框进行图片选择"""
+        img_name, _ = QFileDialog.getOpenFileName(self, '选择要识别的图片', directory='./image', filter='(*.jpg *.png)')
+        if img_name:
+            # 显示图片名
+            self.edit_file.setText(os.path.basename(img_name))
+            pixmap = QPixmap(img_name)
+            # 等比例缩放图片
+            scaled_pixmap = pixmap.scaled(QSize(411, 421), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            # 显示图片
+            self.lb_image.setPixmap(scaled_pixmap)
+            # 识别图像
+            self.recognize()
+
+    def select_type(self):
+        """选择识别的图像类别"""
+        self.image_type = self.cb_type.currentIndex()
+
+    def recognize(self):
+        """图像识别"""
+        print('开始识别图像')
+        result = '识别结果：\n'
+        self.lb_result.setText(result)
+
+    def copy_result(self):
+        """将识别结果复制到剪切板中"""
+        clipboard = QApplication.clipboard()
+        clipboard.setText(self.lb_result.text())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyUi()
+    window.show()
+    sys.exit(app.exec_())
